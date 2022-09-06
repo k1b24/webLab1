111b8ac19f65af0d263caedb5914c28a9914b19b
k1b24 <lazer2004serg@gmail.com>
2022-09-06 20:12:29 +0300

    hotfix

--- src/js/update.js
+++ src/js/update.js
@@ -9,7 +9,7 @@ function recieveSubmit() {
             type: "POST",
             url: "../src/php/index.php",
             async: false,
-            data: {"x": x_value.trim(), "y": y_value.trim(), "r": r_value.trim()},
+            data: {"x": x_value.trim(), "y": y_value.trim().replace(",", "."), "r": r_value.trim()},
             success: function(data) {
                 updateTable(data);
             },
